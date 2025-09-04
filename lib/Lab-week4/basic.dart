5692dc4ba37f31cf89b4205237449992e3e7d7b0
EchoOfHer <6631501037@lamduan.mfu.ac.th>
2025-09-04 14:53:07 +0700

    fea: Add basic app stateless wid

---
+++ lib/Lab-week4/basic.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Basic extends StatelessWidget {
+  const Basic({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('My App')),
+      body: Text('Hello World'),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Text('X'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:lab_4aug/Lab-week4/basic.dart'; //'./Lab-week4/basic.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('My first App')),
-        body: Text('Hello V'),
-        floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          child: Text('X'),
-        ),
-      )
+    MaterialApp(home: 
+      Basic()
     ));
 }
