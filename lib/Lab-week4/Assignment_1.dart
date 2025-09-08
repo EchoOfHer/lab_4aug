e9478b31da34d6d0bb00bae8dbdcd30410ae3b0a
EchoOfHer <6631501037@lamduan.mfu.ac.th>
2025-09-08 20:15:44 +0700

    Add: AppBar

--- lib/Lab-week4/Assignment_1.dart
+++ lib/Lab-week4/Assignment_1.dart
@@ -10,6 +10,14 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Login',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+    );
   }
 }
