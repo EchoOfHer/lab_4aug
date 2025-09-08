ddf0549b846feb0335f81e8a73e4be6f4ec66a7a
EchoOfHer <6631501037@lamduan.mfu.ac.th>
2025-09-08 20:47:48 +0700

    Add : Template

--- lib/Lab-week4/Assignment_1.dart
+++ lib/Lab-week4/Assignment_1.dart
@@ -8,6 +8,15 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final Map<String, dynamic> user_credential = {
+    'user_name': 'admin',
+    'user_password': '1234',
+  };
+  String user_name = '';
+  String user_password = '';
+  TextEditingController tcUser_name = TextEditingController();
+  TextEditingController tcUser_password = TextEditingController();
+  String Login_status = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +26,32 @@ class _LoginState extends State<Login> {
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
         backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 16, right: 16),
+            child: TextField(
+              controller: tcUser_name,
+              decoration: InputDecoration(hintText: 'Username'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16, bottom: 20),
+            child: TextField(
+              controller: tcUser_password,
+              decoration: InputDecoration(hintText: 'Password'),
+            ),
+          ),
+          ElevatedButton(onPressed: null, child: Text('Login')),
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Text(
+              'Lo $Login_status',
+              style: TextStyle(color: Colors.red),
+            ),
+          ),
+        ],
       ),
     );
   }
