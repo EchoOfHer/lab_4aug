22eccc440c455d1b62ada3171c2a67c2afbf1a4d
EchoOfHer <6631501037@lamduan.mfu.ac.th>
2025-09-08 21:39:22 +0700

    input demo

---
+++ lib/Lab-week4/input_demo.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class InputDemo extends StatefulWidget {
+  const InputDemo({super.key});
+
+  @override
+  State<InputDemo> createState() => _InputDemoState();
+}
+
+class _InputDemoState extends State<InputDemo> {
+  String message = '';
+  TextEditingController tcName = TextEditingController();
+  void updatateMessage() {
+    setState(() {
+      message = tcName.text;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Input Demo')),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: TextField(
+              controller: tcName,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Input your name',
+                suffixIcon: IconButton(
+                  onPressed: tcName.clear,
+                  icon: Icon(Icons.clear),
+                ),
+              ),
+            ),
+          ),
+          //SizedBox(height: 16),
+          ElevatedButton(onPressed: updatateMessage, child: Text('OK')),
+          SizedBox(height: 16),
+          Text(message),
+        ],
+      ),
+    );
+  }
+}
