842e41d2f818ef15457e647ec0f58a1da2e20c5e
EchoOfHer <6631501037@lamduan.mfu.ac.th>
2025-09-08 22:02:58 +0700

    Template

--- lib/Lab-week4/Assignment_2.dart
+++ lib/Lab-week4/Assignment_2.dart
@@ -8,6 +8,12 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+  //-------------------------------- Variable
+  String first_num = '';
+  String second_num = '';
+  String output = '';
+  TextEditingController tcFirst_num = TextEditingController();
+  TextEditingController tcSecond_num = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +26,51 @@ class _CalculatorState extends State<Calculator> {
         ),
         backgroundColor: Colors.blue,
       ),
-      body: ,
+      body: Column(
+        children: [
+          //--------------- text field '2nd number'
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 16, right: 16),
+            child: TextField(
+              controller: tcFirst_num,
+              decoration: InputDecoration(hintText: 'Number 1'),
+            ),
+          ),
+          //--------------- text field '2nd number'
+          Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16, bottom: 20),
+            child: TextField(
+              controller: tcSecond_num,
+              decoration: InputDecoration(hintText: 'Number 2'),
+            ),
+          ),
+          //--------------- Summation
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 247, 173, 63),
+            ),
+            child: Text('Sum', style: TextStyle(color: Colors.white)),
+          ),
+
+          SizedBox(height: 16),
+          //--------------- Power
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+            child: Text('Power', style: TextStyle(color: Colors.white)),
+          ),
+          SizedBox(height: 16),
+          //--------------- Clear
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+            child: Text('Clear', style: TextStyle(color: Colors.white)),
+          ),
+          SizedBox(height: 16),
+          Text('l$output', style: TextStyle(color: Colors.red)),
+        ],
+      ),
     );
   }
 }
