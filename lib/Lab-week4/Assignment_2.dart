10cb6fd3b42b0c9ee756c9bed4ba2f30b805763a
EchoOfHer <6631501037@lamduan.mfu.ac.th>
2025-09-08 21:47:16 +0700

    Add : Appbar

---
+++ lib/Lab-week4/Assignment_2.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator'),
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 24,
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: ,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-//import 'package:lab_4aug/Lab-week4/basic.dart';
-import 'package:lab_4aug/Lab-week4/input_demo.dart'; //'./Lab-week4/basic.dart';
+import 'package:lab_4aug/Lab-week4/Assignment_2.dart'; //'./Lab-week4/basic.dart';
 
 void main() {
-  runApp(MaterialApp(home: InputDemo(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: Calculator(), debugShowCheckedModeBanner: false));
 }
