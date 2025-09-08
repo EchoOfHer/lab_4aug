c676666bbb29f4cd63400a9603df016b2b136eab
EchoOfHer <6631501037@lamduan.mfu.ac.th>
2025-09-08 19:57:06 +0700

    make an template

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 //import 'package:lab_4aug/Lab-week4/basic.dart';
-import 'package:lab_4aug/Lab-week4/counter.dart'; //'./Lab-week4/basic.dart';
+import 'package:lab_4aug/Lab-week4/input_demo.dart'; //'./Lab-week4/basic.dart';
 
 void main() {
-  runApp(MaterialApp(home: 
-    Counter()
-  ));
+  runApp(MaterialApp(home: InputDemo(), debugShowCheckedModeBanner: false));
 }
