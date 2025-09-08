a0b3ebf6867b546d821d86912c975e2e390cb0b3
EchoOfHer <6631501037@lamduan.mfu.ac.th>
2025-09-08 19:58:56 +0700

    Add : File for assignment

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:lab_4aug/Lab-week4/Assignment_1.dart';
 //import 'package:lab_4aug/Lab-week4/basic.dart';
-import 'package:lab_4aug/Lab-week4/input_demo.dart'; //'./Lab-week4/basic.dart';
+// import 'package:lab_4aug/Lab-week4/input_demo.dart'; //'./Lab-week4/basic.dart';
 
 void main() {
-  runApp(MaterialApp(home: InputDemo(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: Login(), debugShowCheckedModeBanner: false));
 }
