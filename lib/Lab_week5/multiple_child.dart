faaed0f5a20a01c9159a9da6905c606d4528edf9
EchoOfHer <6631501037@lamduan.mfu.ac.th>
2025-09-11 15:39:18 +0700

    Add  : collumn

--- lib/Lab_week5/multiple_child.dart
+++ lib/Lab_week5/multiple_child.dart
@@ -7,29 +7,13 @@ class MultipleChild extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Row(
-          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          // children: [
-          //   Container(color: Colors.green, width: 100, height: 100),
-          //   //custom alignment
-          //   Spacer(flex: 1),
-          //   Container(color: Colors.yellow, width: 100, height: 150),
-          //   Spacer(flex: 2),
-          //   Container(color: Colors.red, width: 100, height: 100),
-          // ],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            //fit to remaing spcace
-            Expanded(
-              flex: 1,
-              child: Container(color: Colors.green, width: 100, height: 100),
-            ),
-            Container(color: Colors.yellow, width: 100, height: 150),
-
-            Expanded(
-              flex: 2,
-              child: Container(color: Colors.red, width: 100, height: 100),
-            ),
+            Container(color: Colors.green, width: 100, height: 100),
+            Container(color: Colors.yellow, width: 150, height: 100),
+            Container(color: Colors.red, width: 100, height: 100),
           ],
         ),
       ),
